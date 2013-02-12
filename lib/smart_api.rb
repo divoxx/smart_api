77d07aa12c2a57baa630538eb8604c96b4b58beb
Rodrigo Kochenburger <divoxx@gmail.com>
2013-02-12 13:04:40 -0800

    Better format/indent autoload block

--- lib/smart_api.rb
+++ lib/smart_api.rb
@@ -2,6 +2,6 @@ require "smart_api/engine"
 
 module SmartApi
   autoload :EndpointDescriptor, "smart_api/endpoint_descriptor"
-  autoload :Dsl, "smart_api/dsl"
-  autoload :Controller, "smart_api/controller"
+  autoload :Dsl,                "smart_api/dsl"
+  autoload :Controller,         "smart_api/controller"
 end
